2093f8a9d5939d3000db774d6b91c4e80c9b2f12
IsaqueBatista <isaquebatista1716@gmail.com>
2024-03-07 18:17:59 -0300

    Pergunta 1

---
+++ index.js
@@ -0,0 +1,15 @@
+// Estágio Ribeirão Preto - 2024 - ISAQUE BATISTA DE OLIVEIRA
+
+// PERGUNTA 1
+
+let INDICE = 13;
+let SOMA = 0;
+let K = 0;
+
+while (K < INDICE) {
+    K = K + 1;
+    SOMA = SOMA + K;
+}
+
+console.log(`Pergunta 1, resposta: ${SOMA}`);
+// Resultado do processamento é 91
